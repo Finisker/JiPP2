b08488a3ddaffbe41c0a3d4ab97bff41c9cf4dc5
Maciek <macpla08@gmail.com>
2021-10-17 19:14:05 +0200

    Dodałem dokumentację do funkcji w pliku "calc.cpp"

--- simpleCalc/src/calc.cpp
+++ simpleCalc/src/calc.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 
 using namespace std;
+/* Dodawanie dwóch liczb całkowitych
+ * @param a - pierwsza dodawana liczba
+ * @param b - druga dodawana liczba
+ */
 int add(int a, int b){
     return a+b;
 }
-
+/* Odejmowanie dwóch liczb całkowitych
+ * @param a - odjemna
+ * @param b - odjemnik
+ */
 int subtract(int a, int b){
     return a-b;
 }
-
+/* Obliczanie objętości graniastosłupa prostego o podstawie trapezu
+ * @param a - pierwsza podstawa trapezu
+ * @param b - druga podstawa trapezu
+ * @param h - wysokość graniastosłupa
+ */
 double volume(int a, int b, int h){
     return ((a+b)/2.)*h;
 }
-
+/* Wyświetlanie dokumentacji
+ */
 void help(){
     cout<<"Simple calculator\n"
           "simpleCalc [nazwa działania]\n\n"
