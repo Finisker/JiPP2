7d88ec5306b3f9a10cbe1ff9fc5f80d615a03e65
Finisker <macpla08@gmail.com>
2021-11-08 11:01:46 +0100

    Did point 4 from 1.2

---
+++ Lab5/Zad1/include/Car.h
@@ -0,0 +1,30 @@
+//
+// Created by milky on 08.11.2021.
+//
+
+#ifndef JIPP2_CAR_H
+#define JIPP2_CAR_H
+
+#include "iostream"
+#include "cstring"
+
+using namespace std;
+
+class Car {
+private:
+    int registrationNumber;
+    int seatsNumber;
+
+    string name;
+    string markAndType;
+
+    string* passengers = new string[seatsNumber];
+
+public:
+    Car(int registrationNumber, int seatsNumber, string name, string markAndType);
+
+    void print();
+};
+
+
+#endif //JIPP2_CAR_H
---
+++ Lab5/Zad1/src/Car.cpp
@@ -0,0 +1,27 @@
+//
+// Created by milky on 08.11.2021.
+//
+
+#include "../include/Car.h"
+
+Car::Car(int registrationNumber, int seatsNumber, string name, string markAndType) {
+    this->registrationNumber = registrationNumber;
+    this->seatsNumber = seatsNumber;
+    this->name = name;
+    this->markAndType = markAndType;
+    for(int i=0; i<seatsNumber; ++i){
+        passengers[i]="Puste";
+    }
+}
+
+void Car::print() {
+    cout<<"Nazwa: "<<name<<endl<<
+    "Marka i typ: "<<markAndType<<endl
+    <<"Numer rejestracyjny: "<<registrationNumber<<endl
+    <<"Ilość miejsc: "<<seatsNumber<<endl;
+
+    for(int i=0; i<seatsNumber; ++i){
+        cout<<"Pasażer "<<i+1<<" : "<<passengers[i]<<endl;
+    }
+}
+
